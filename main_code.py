ce3f4339c043608409d6920e7c0df2cc216c0437
gage-emery <111905613+gage-emery@users.noreply.github.com>
2024-02-06 09:06:00 -0600

    main_code.py

--- main_code.py
+++ main_code.py
@@ -21,25 +21,25 @@ def questionsForAnimal(goAgain,score):
         questionAns = ""
         questionAns = input("Do you like exotic pets? (y/n)")
 
-print("Do you have a very busy schedule? Answer y or n.")
+        print("Do you have a very busy schedule? Answer y or n.")
         
-print("Do you like exotic pets? Answer y ot n")
-if answer == "y":
-    scoreAdder(score,"weak")
+        print("Do you like exotic pets? Answer y ot n")
+        if answer == "y":
+            scoreAdder(score,"weak")
 
-if answer == "n":
-    scoreAdder(score,"med")
+        if answer == "n":
+            scoreAdder(score,"med")
 
-print("if you would like a high maitnence pet answer y, if you would like a low maitnence pet answer n.")
-if answer == "y":
-    scoreAdder(score,"weak")
+        print("if you would like a high maitnence pet answer y, if you would like a low maitnence pet answer n.")
+        if answer == "y":
+            scoreAdder(score,"weak")
 
-if answer == "n":
-    scoreAdder(score,"str")
+        if answer == "n":
+            scoreAdder(score,"str")
 
-print("Do you live in a warmer climate? answer y. Do you live in a colder climate? answer n")
-if answer == "y":
-    scoreAdder(score,"weak")
+        print("Do you live in a warmer climate? answer y. Do you live in a colder climate? answer n")
+        if answer == "y":
+            scoreAdder(score,"weak")
 
-if answer == "n": 
-    scoreAdder(score,"str")
+        if answer == "n": 
+            scoreAdder(score,"str")
